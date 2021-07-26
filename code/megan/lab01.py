52757092f8224cba406902297c3af8030aaa8b0a
meganbradner <meganrbradner@gmail.com>
2021-07-26 16:04:19 -0700

    added new file

---
+++ code/megan/lab01.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+penup()
+setposition(100, 100)
+pendown()
+
+i = 0
+while i < 100:
+    forward(2)
+    left(360/100)
+    i = i + 1
+
+penup()
+setposition(50, 100)
+pendown()
+
+fillcolor('pink')
+begin_fill()
+
+forward(200)
+left(120)
+forward(200)
+left(120)
+forward(200)
+left(120)
+forward(200)
+left(120)
+
+end_fill()
+
+done()
