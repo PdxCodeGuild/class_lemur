7d01b5b26ca29740f9329450dfc5c3c7de0a0e78
demesimms <simmons.demetric@gmail.com>
2021-07-26 18:40:41 -0400

    turtle lab

---
+++ code/deme/lab1.py
@@ -0,0 +1,20 @@
+from turtle import *
+#head
+head = 50
+circle(head)
+
+#body
+forward(100)
+right(180)
+forward(200)
+right(180)
+forward(100)
+right(90)
+forward(100)
+right(45)
+forward(100)
+right(180)
+forward(100)
+right(90)
+forward(100)
+done()
