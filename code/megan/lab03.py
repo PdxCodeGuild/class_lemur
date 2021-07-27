a81e88c7e4c902b7e64e6c71bb2e02583c90dbe6
meganbradner <meganrbradner@gmail.com>
2021-07-27 15:52:09 -0700

    updated lab

--- code/megan/lab03.py
+++ code/megan/lab03.py
@@ -1,1 +1,17 @@
-input("Please enter a dollar amount: ")
+cents = float(input("Please enter a dollar amount: "))
+
+quarter = cents // 25
+dimes = cents % 25
+
+dime = dimes // 10
+nickels = dimes % 10
+
+nickel = nickels // 5
+pennies = nickels % 5
+
+penny = cents // 1
+
+print(f"You have {quarter} quarters")
+print(f"You have {dimes} dimes")
+print(f"You have {nickels} nickles")
+print(f"You have {pennies} pennies")
