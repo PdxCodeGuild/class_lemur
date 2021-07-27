2d37e0c788f1cf89533aecd27e839f70094943a5
David Swartwood <david@willamettebrothers.com>
2021-07-27 11:40:10 -0700

    Committer: David Swartwood <david.swartwood@att.net> 	modified:   lab2.py

--- code/swarty/lab2.py
+++ code/swarty/lab2.py
@@ -5,18 +5,27 @@ We're going to average a list of numbers. Start with the following list, iterate
 keeping a 'running sum', then divide that sum by the number of elements in that list. 
 Remember len will give you the length of a list."""
 
+#nums = [5, 0, 8, 3, 4, 1, 6]
+nums= []
+while True:
+    added=input("enter a number for the list (if done hit enter):  ")
+    if added == "":
+        break
+    added = int(added)
+    nums.append(added)
+print(nums)
 
-nums = [5, 0, 8, 3, 4, 1, 6]
+
 total=0
 # loop over the elements
-for num in nums:
-    total += num
-    print(num, total)
+#for num in nums:
+#    total += num
+#    print(num, total)
     
     
 
 # loop over the indices
 for i in range(len(nums)):
-    print(nums[i])
-    total += num
+#    print(nums[i])
+    total += nums[i]
 print(total/(i+1))
