2c5123c6240b1957babefc8cbca4fca991065bd7
dbrunken <david.brunken@startmail.com>
2021-07-27 12:12:00 -0700

    current progress of averagenumbers lab

---
+++ code/dbrunken/practice_work/averagenumbers.py
@@ -0,0 +1,39 @@
+'''
+create an average of numbers
+copied code from lab
+'''
+
+# nums = [5, 0, 8, 3, 4, 1, 6]
+
+# # loop over the elements
+# # for num in nums:
+# #     print(num)
+
+# # loop over the indices
+# for i in range(len(nums)):
+#     print(nums[i])
+
+# run_sum = sum(nums) #/ len(nums)
+
+# print(run_sum)
+
+# -------------------------------------------------------------------------
+
+nums = []
+
+user_num = input('input a number or type "done": ')
+user_num = int(user_num)
+nums = nums.append(user_num)
+print(type(nums))
+
+
+
+# while nums < 0:
+#     if input == 'done':
+#         print(sum(nums / len(nums)))
+#         break
+
+
+
+
+
