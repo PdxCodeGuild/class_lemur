7db0ea1f7baf8006b1e98aaf00694e59ca1df040
David Swartwood <david.swartwood@att.net>
2021-07-27 14:21:29 -0700

    modified:   lab3.py

--- code/swarty/lab3.py
+++ code/swarty/lab3.py
@@ -17,12 +17,8 @@ coins = [
 
 total_cash=input("enter the cash amount in $.$$ format: ")
 pennies=100 * float(total_cash)
-change=[]
 for coin in coins:
     print(f"There are {int(pennies//(coin[1]))} {coin[0]} ") #print # of the current coin
-#    change.append(round(pennies//(coin[1])))
     pennies = pennies%coin[1]                       #Change Remining cents
-#print(change)
 
-#print("change is: ", change[0], coins[0], change[1], change[2], "nickels", change[3], "pennies")
 
