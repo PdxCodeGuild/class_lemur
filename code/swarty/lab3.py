76caceb16c4af635a3a59f159248584114800252
David Swartwood <david.swartwood@att.net>
2021-07-27 14:13:32 -0700

    modified:   lab3.py

--- code/swarty/lab3.py
+++ code/swarty/lab3.py
@@ -4,8 +4,15 @@ Let's convert a dollar amount into a number of coins. The input will be the tota
 the output will be the number of quarters, dimes, nickles, and pennies. 
 Always break the total into the highest coin value first, resulting in the fewest amount of coins. 
 """
+coins = [
+    ('half-dollar', 50),
+    ('quarter', 25),
+    ('dime', 10),
+    ('nickel', 5),
+    ('penny', 1)
+]
 
-coins = [25,10,5,1]
+#coins = [25,10,5,1]
 
 
 total_cash=input("enter the cash amount in $.$$ format: ")
@@ -14,16 +21,10 @@ change=[]
 for coin in coins:
 #   if pennies%coin == 0:
 #       break
-    change.append(pennies//coin)
-    pennies = pennies%coin
-print(change)
+    print(f"There are {int(pennies//(coin[1]))} {coin[0]} ")
+#    change.append(round(pennies//(coin[1])))
+    pennies = pennies%coin[1]
+#print(change)
 
-pennies=100 * float(total_cash)
-quarters = pennies//25
-pennies = pennies%25
-dimes = pennies//10
-pennies = pennies%10
-nickels = pennies//5
-pennies = pennies%5
+#print("change is: ", change[0], coins[0], change[1], change[2], "nickels", change[3], "pennies")
 
-print("change is: ", quarters, "quarters", dimes, "dimes", nickels, "nickels", pennies, "pennies")
