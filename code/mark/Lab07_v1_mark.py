389d16c91ca5252d24a9ae3f8630582b967ab279
Mark Dziuban <markdziuban@gmail.com>
2021-07-28 17:41:25 -0400

    Lab 7 complete

--- code/mark/Lab07_v1_mark.py
+++ code/mark/Lab07_v1_mark.py
@@ -1,18 +1,16 @@
 def credit_card_validation(card_num_string):
+    reversed_as_int = []
     card_num_list = list(card_num_string)
     check_digit = card_num_list.pop()
     reversed_card_num = card_num_list[::-1]
-    print(reversed_card_num)
     for num in reversed_card_num:
-        num = int(num)
-    print(reversed_card_num)
-    for i in range(0, len(reversed_card_num), 2):
-        reversed_card_num[i] *= 2
-        if reversed_card_num[i] > 9:
-            reversed_card_num[i] -= 9
-    print(reversed_card_num)
-    sum_of_list = sum(reversed_card_num)
-    print(sum_of_list)
+        reversed_as_int.append(int(num))
+    for i in range(0, len(reversed_as_int), 2):
+        reversed_as_int[i] *= 2
+        if reversed_as_int[i] > 9:
+            reversed_as_int[i] -= 9
+    sum_of_list = sum(reversed_as_int)
+    sum_of_list = str(sum_of_list)
     second_digit_of_sum = sum_of_list[1]
     if second_digit_of_sum == check_digit:
         print("Valid!")
@@ -21,4 +19,4 @@ def credit_card_validation(card_num_string):
     
         
 
-credit_card_validation("123456789")
+credit_card_validation("455673758689985")
