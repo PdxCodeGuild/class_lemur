bb6562caf85351db367d7c007fd5f72657bc10be
Scott Cormack <cormackscott@hotmail.com>
2021-07-29 17:19:20 -0400

    Attempt at version 2

--- code/dbrunken/practice_work/jackalope_mark_scott_dbrunk.py
+++ code/dbrunken/practice_work/jackalope_mark_scott_dbrunk.py
@@ -4,18 +4,28 @@ Scott Cormack
 Mark Dziuban
 David Brunken
 '''
-
+import random
+from string import ascii_letters
 
 #set variables for population, years, population goal, death age
 
 year = 0
-population = [0,0] #age 0 jacks
+# population = [0,0] #age 0 jacks
 jack_age = 0
 breed_age = [4, 8]
 death_age = 10
 # spawn =
 pop_goal = 1000 #population goal
 
+
+def jack_creation():
+    jack_dict = { }
+    list_letters = list(ascii_letters)
+    jack_dict['name'] = random.choice(list_letters)
+    jack_dict['age'] = 0
+    jack_dict['sex'] = random.choice(['m','f'])
+    jack_dict['pregnant'] = False
+    return jack_dict
 
 def jack_pop(population, year):
     while len(population) < pop_goal:
@@ -45,5 +55,15 @@ def jack_pop(population, year):
 
 
     print(f"Number of years: {year} with a population of {len(population)}")
+
+def add_pop(population, num_to_add):
+    for i in range(num_to_add):
+        population.append(jack_creation())
+    return population
+
+
+def main():
+    population = []
+    population = add_pop(population, 2)
 
 jack_pop(population,year)
