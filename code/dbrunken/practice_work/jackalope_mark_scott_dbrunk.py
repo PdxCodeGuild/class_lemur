0ed995c0bf4dd0a6d97290894cea4e683b28094d
Mark Dziuban <markdziuban@gmail.com>
2021-07-29 15:31:38 -0400

    finished v1

--- code/dbrunken/practice_work/jackalope_mark_scott_dbrunk.py
+++ code/dbrunken/practice_work/jackalope_mark_scott_dbrunk.py
@@ -17,21 +17,33 @@ death_age = 10
 pop_goal = 1000 #population goal
 
 
+def jack_pop(population, year):
+    while len(population) < pop_goal:
+        num_of_breeders = 0
+        baby_jacks = 0
 
-while len(population) < pop_goal:
-    num_of_breeders = 0
-    baby_jacks = 0
+        while death_age in population:
+            population.remove(death_age)
+        
 
-    while death_age in population:
-        population.remove(death_age)
-    
+        for jack in population:
+            if jack < breed_age[1] and jack > breed_age[0]:
+                num_of_breeders += 1
 
-    for jack in population:
-        if jack < breed_age[1] and jack > breed_age[0]:
-            num_of_breeders += 1
+        baby_jacks = num_of_breeders // 2 * 2
+        for i in range(baby_jacks):
+            population.append(0)
+            i += 1
+        
+        year += 1
 
-    baby_jacks = num_of_breeders // 2 * 2
-    for baby in baby_jacks:
-        population.append(0)
+        for i in range(len(population)):
+            population[i] += 1
+            
+        
+        #print(f"Number of years: {year} with a population of {population}")
 
-# print(population)
+
+    print(f"Number of years: {year} with a population of {len(population)}")
+
+jack_pop(population,year)
