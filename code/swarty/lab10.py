f7630b4e8ec550be3bc351c55bc39624813a8a48
David Swartwood <david.swartwood@att.net>
2021-07-29 16:23:20 -0700

    modified:   lab10.py 	modified:   lab8.py 	modified:   lab9.py

--- code/swarty/lab10.py
+++ code/swarty/lab10.py
@@ -1,5 +1,5 @@
 """
-Lab 8 David Swartwood
+Lab 10 David Swartwood
 
 Write a program that prompts the user for a string, and encodes it with ROT13. For each character, find the corresponding character, 
 add it to an output string. Notice that there are 26 letters in the English language, so encryption is the same as decryption.
