26ab438c8703edf5c0c0c4fa1795c6b28df3130a
David Swartwood <david.swartwood@att.net>
2021-07-28 21:29:59 -0700

    new file:   lab7.py

---
+++ code/swarty/lab7.py
@@ -0,0 +1,40 @@
+"""
+Lab 7 David Swartwood
+Let's write a function which returns whether a string containing a credit card number is valid as a boolean. The steps are as follows:
+
+Convert the input string into a list of ints
+Slice off the last digit. That is the check digit.
+Reverse the digits.
+Double every other element in the reversed list.
+Subtract nine from numbers over nine.
+Sum all values.
+Take the second digit of that sum.
+If that matches the check digit, the whole card number is valid.
+"""
+
+
+
+cc=input("Enter your Credit Card #: ")
+#cc=("4556737586899855")
+if len(cc) != 16:
+    cc=input("Incorrect entry \nEnter your Credit Card #: ")
+cc=list(cc)
+ccintegers=[int(num) for num in cc]
+cccheck=ccintegers.pop()
+ccintegers.reverse()
+for i in range(len(ccintegers)):
+    if i%2 ==0:
+        ccintegers[i]+=ccintegers[i]
+ccsum=0
+for num in ccintegers:
+    print(num)
+    if num>9:
+        num-=9
+    print(num)
+    ccsum+=num
+if ccsum%10 == cccheck:
+    print("CC number is valid")
+else:
+    print("card not valid")
+
+print(ccsum, ccintegers, cccheck)
