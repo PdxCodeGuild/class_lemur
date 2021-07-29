85fb844434fd73803fe9d64790cd5045182d3e46
David Swartwood <david.swartwood@att.net>
2021-07-29 13:45:37 -0700

    modified:   ../swarty/lab6.py

--- code/swarty/lab6.py
+++ code/swarty/lab6.py
@@ -12,7 +12,11 @@ After the loop, print the final balance
 
 V2 - add ROI
 """
+from random import randint
+import decimal
+
 prizes={
+    0: 0,
     1: 4,
     2: 7,
     3: 100,
@@ -21,18 +25,29 @@ prizes={
     6: 25000000
 }
 
-from random import randint
+
 def pick6():
     numbers=[]
     for i in range(6):
-        numbers.append(randint(1,99))
+        numbers.append(randint(1,20))
     return numbers
 
-def num_match(winning=pick6(), ticket=pick6()):
+def num_match(winning, ticket):
     matches=0
     for i in range(6):
         if winning[i]==ticket[i]:
             matches+=1
     cash=prizes[matches]
+    print(matches,cash,winning,ticket)
     return matches, cash
-print(num_match())
+cost=0
+won=0
+luck=0
+for i in range(100):
+    cost+=2
+    ticket_luck,ticket_win=num_match(pick6(),pick6())
+    luck+=ticket_luck
+    won+=ticket_win
+
+print(f'You spent ${cost} and won ${won}\nYou picked correctly {round((luck/6),1)}% of the time\nYour ROI was {round(((won-cost)/cost)*100,1)}%')   
+
