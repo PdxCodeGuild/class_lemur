10ec9a4e5157bcde06f5d2e6676c963813af712e
meganbradner <meganrbradner@gmail.com>
2021-07-30 16:31:45 -0700

    added changes

--- code/megan/lab07.py
+++ code/megan/lab07.py
@@ -1,5 +1,29 @@
-number = [int(input("Please enter your credit card number: "))]
+number = list(input("Please enter your 16 digit credit card number: "))
 
-print(number)
+digits = [int(i) for i in number]
 
+digits_minus_one = digits[0:15]
+
+digits_reversed = digits_minus_one[::-1]
+print(digits_reversed)
+ 
+doubled = []
+
+for every_other in digits_reversed:
+    if every_other % 2 == 0:
+        doubled.append(every_other * 2)
+
+print(doubled)
+
+for digit in digits_reversed:
+    if digit < 9:
+        print(digit - 9)
+
+last = sum(digits_reversed)
+print(last)
+
+if 8 == digits[15]:
+    print("This is a valid credit card number.")
+else:
+    print("Sorry. This is not a valid credit card number.")
 
