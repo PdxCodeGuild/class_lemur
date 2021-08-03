52f3908da30bac12a4893e77b019349825a66d8c
demesimms <simmons.demetric@gmail.com>
2021-08-03 18:58:05 -0400

    updated lab 7 and lab 1

--- code/deme/lab1.py
+++ code/deme/lab1.py
@@ -18,13 +18,3 @@ forward(100)
 right(90)
 forward(100)
 done()
-
-
-7901 Kenwood
-1st Floor North: 0701 - 0722
-1st Floor South B: 0726 - 0800
-1st Floor South A:  0801 - 0839
-2nd Floor North: 0842 - 0910
-2nd Floor South B: 0917 - 0951
-2nd Floor South A: 0952 - 1026
-3rd Floor North: 1028 - 1056
