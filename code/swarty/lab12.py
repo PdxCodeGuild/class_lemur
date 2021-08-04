7daff15a4d8bcf4a8672c052b3d96d045405d000
David Swartwood <david.swartwood@att.net>
2021-08-04 12:19:49 -0700

    modified:   DavidsData.csv 	modified:   lab12.py

--- code/swarty/lab12.py
+++ code/swarty/lab12.py
@@ -1,5 +1,5 @@
 """
-Lab12 - Contact LIst - David Swartwood 
+Lab12 - Contact List - David Swartwood 
 
 Let's build a program to manage a list of contacts. To start, we'll build a CSV ('comma separated values') together, 
 and go over how to load that file. Headers might consist of name, favorite fruit, favorite color. Open the CSV, 
@@ -11,10 +11,23 @@ the keys, the text in the other lines represent the values.
 
 
 """
-
-with open('DavidsData.csv', 'w') as file:
-    lines = file.read().split('\n')
-    print(lines)
+#import modules
+from datetime import datetime
 
 
+file='../../code/swarty/DavidsData.csv'
+updated=f'../../code\swarty\DavidsData_{datetime.now().strftime("%b%d_%Y")}.csv'
+#Read current file
+lines={}
+with open(file, 'r') as file:
+    lines=file.read().split('\n')
+    
+print(lines)
 
+
+
+
+
+#Write to file
+# with open(updated, 'w') as file:
+#     file.write('\nHyundai, mid market, small cars and SUVs, South Korea, Hyundai Motor Company'.join(line))
