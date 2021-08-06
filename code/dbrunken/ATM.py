08df9eebc4143b4e7f6303ae531860f16e251afe
dbrunken <david.brunken@startmail.com>
2021-08-06 12:19:06 -0700

    finished atm

--- code/dbrunken/ATM.py
+++ code/dbrunken/ATM.py
@@ -9,23 +9,27 @@ class ATM:
         self.interest_rate = interest_rate
 
     def check_balance(self):
-        print(self.balance)
+         return self.balance
 
     def deposit(self, amount):
         self.balance += amount
-        print()
+        # print()
         
     def check_withdrawl(self, amount):
         for amount in self.balance:
-            
+            self.balance != amount
+            if amount < self.balance:
+                return False
+            else:
+                return True
 
     def withdrawl(self, amount):
         self.balance -= amount
-        print(self.balance)
+        return self.balance
 
     def calc_interest(self):
         interest = self.balance * self.interest_rate
-        print(interest)
+        return interest
 
 atm = ATM()
 print("Welcome to Communist Chase ATM")
@@ -39,6 +43,10 @@ while True:
         print('interest - accumulate interest')
         print('exit     - exit the program')
     
+    elif command == 'balance':
+        my_balance = atm.check_balance()  # call the check_balance() method
+        print(f'Your balance is ${my_balance}')
+    
     elif command == 'deposit':
         amount = float(input('Enter deposit amount: '))
         atm.deposit(amount)
@@ -46,14 +54,14 @@ while True:
 
     elif command == 'withdraw':
         amount = float(input('How much you want: '))
-        if atm.check_balance(amount):
+        if atm.check_balance():
             atm.withdrawl(amount)
             print(f'Withdrew ${amount}')
         else:
             print('Cant afford it')
     elif command == 'interest':
         amount = atm.calc_interest()
-        atm.deposit(amount)
+        # atm.deposit(amount)
         print(f"Managed ${amount} in interest")
     elif command == 'exit':
         break
