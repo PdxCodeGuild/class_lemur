a1a431d8d9dce6665a7267363a4c7627741f88fb
Mark Dziuban <markdziuban@gmail.com>
2021-08-06 15:22:09 -0400

    Completed Connect 4 v2

--- code/mark/Connect4_v2_mark.py
+++ code/mark/Connect4_v2_mark.py
@@ -1,14 +1,18 @@
-from os import truncate
-
-
 class Player:
     def __init__(self, name, color):
         self.name = name
         self.color = color
 
 class Game:
-    def __init__(self, board=[], moves=0):
-        self.board = board
+    def __init__(self, moves=0):
+        self.board = [
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ']
+        ]
         self.moves = moves
 
     def get_height(self, position):
@@ -23,8 +27,45 @@ class Game:
         self.board[height][position] = player.color
         self.moves += 1
 
+    def check_if_valid_move(self, position):
+        if self.board[0][position-1] == ' ':
+            return True
+        else:
+            return False
+
     def calc_winner(self):
-        pass
+        directions = ['across', 'vertical', 'diagonal_up', 'diagonal_down']
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                for direction in directions:
+                    match_count, pos_1, pos_2 = self.check_recursion(i,j,direction)
+                    if match_count >= 3:
+                        return True, pos_1, pos_2
+        return False 
+
+    def check_recursion(self,pos_1, pos_2, direction,match_count=0):
+        if self.check_for_match(pos_1,pos_2, direction, match_count+1) == True:
+            return self.check_recursion(pos_1, pos_2, direction, match_count+1)
+        else:
+            return match_count, pos_1, pos_2
+
+
+    def check_for_match(self, pos_1, pos_2, direction, match_count):
+        if direction == 'across':
+            position_list = [pos_1, pos_2, pos_1, pos_2+match_count]
+        elif direction == 'vertical':
+            position_list = [pos_1, pos_2, pos_1+match_count, pos_2]
+        elif direction == 'diagonal_up':
+            position_list = [pos_1, pos_2, pos_1+match_count, pos_2+match_count]
+        elif direction == 'diagonal_up':
+            position_list = [pos_1, pos_2, pos_1-match_count, pos_2+match_count]
+        try:
+            if self.board[position_list[0]][position_list[1]] == self.board[position_list[2]][position_list[3]]:
+                return True
+        except:
+            return False
+        else:
+            return False
 
     def is_full(self):
         if self.moves == 42:
@@ -55,19 +96,9 @@ def initialize_players():
     player_2 = Player(player_2_name, player_2_color)
     return player_1, player_2
 
-
-board = [
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ']
-        ]
-
-game = Game(board)
+game = Game()
         
-def main():
+if __name__ == '__main__':
     list_of_players = list(initialize_players())
     while True:
         print(f"\nIt is {list_of_players[game.moves%2].name}'s turn")
@@ -76,18 +107,26 @@ def main():
                 player_move = int(input("\nSelect a column to place a piece (1-7): "))
                 if player_move > 7 or player_move < 1:
                     raise ValueError
-                else:
-                    break
+                #else:
+                    #break
             except ValueError:
                 print("That is not a valid entry.")
                 continue
+            if game.check_if_valid_move(player_move) == True:
+                game.move(list_of_players[game.moves%2], player_move-1)
+                break
+            else:
+                print("\nThat column is full.  Select another column.")
+                continue
 
-
-        game.move(list_of_players[game.moves%2], player_move-1)
         for line in game.board:
             print("|".join(line))
         if game.is_game_over() == True:
+            result, pos_1, pos_2 = game.calc_winner()
+            if result == True:
+                print(f"You win!\nWinning connect 4 starting at {pos_1},{pos_2}")
+            else:
+                print("No winner!")
             break
 
-main()
 
