460cab3456beaf6dde0c8fdcda47826b28b6f53d
meganbradner <meganrbradner@gmail.com>
2021-08-09 16:30:14 -0700

    added lab

---
+++ code/megan/lab15.py
@@ -0,0 +1,40 @@
+'''Part One: Linear Search'''
+
+def linear_search(nums, value):
+    for i in range(len(nums)):
+        if nums[i] == value:
+            return i
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+index = linear_search(nums, 3)
+print(index) 
+
+
+'''Part Two: Binary Search'''
+
+def binary_search(nums, value):
+    low = 0
+    high = len(nums)
+    mid = int(len(nums) / 2)
+
+
+    while low < high:
+
+        for i in range(len(nums)):
+            if nums[i] == value:
+                return i
+
+            if mid == value:
+                return mid[i]
+
+            elif value > mid:
+                low = mid
+                
+            elif value < mid:
+                high = mid
+        
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+index = binary_search(nums, 4)
+print(index)
