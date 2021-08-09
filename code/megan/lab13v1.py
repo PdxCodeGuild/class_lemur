9f4897c458d06529d5388fad358f2302c2c3d39c
meganbradner <meganrbradner@gmail.com>
2021-08-09 12:14:22 -0700

    mob lab

--- code/megan/lab13v1.py
+++ code/megan/lab13v1.py
@@ -32,15 +32,19 @@ class ATM:
         
 atm = ATM()  # create an instance of our class
 print('Welcome to the ATM')
+
 while True:
     command = input('Enter a command: ')
+
     if command == 'balance':
         balance = atm.check_balance()  # call the check_balance() method
         print(f'Your balance is ${balance}')
+
     elif command == 'deposit':
         amount = float(input('How much would you like to deposit? '))
         atm.deposit(amount)  # call the deposit(amount) method
         print(f'Deposited ${amount}')
+
     elif command == 'withdraw':
         amount = float(input('How much would you like? '))
         # call the check_withdrawal(amount) method
@@ -49,10 +53,12 @@ while True:
             print(f'Withdrew ${amount}')
         else:
             print('Insufficient funds')
+
     elif command == 'interest':
         amount = atm.calc_interest()  # call the calc_interest() method
         atm.deposit(amount)
         print(f'Accumulated ${amount} in interest')
+
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
@@ -60,7 +66,9 @@ while True:
         print('withdraw - withdraw money')
         print('interest - accumulate interest')
         print('exit     - exit the program')
+
     elif command == 'exit':
         break
+    
     else:
         print('Command not recognized')
--- code/megan/lab13v2.py
+++ code/megan/lab13v2.py
@@ -38,15 +38,19 @@ class ATM:
 
 atm = ATM()  # create an instance of our class
 print('Welcome to the ATM')
+
 while True:
     command = input('Enter a command: ')
+
     if command == 'balance':
         balance = atm.check_balance()  # call the check_balance() method
         print(f'Your balance is ${balance}')
+
     elif command == 'deposit':
         amount = float(input('How much would you like to deposit? '))
         atm.deposit(amount)  # call the deposit(amount) method
         print(f'Deposited ${amount}')
+
     elif command == 'withdraw':
         amount = float(input('How much would you like? '))
         # call the check_withdrawal(amount) method
@@ -55,13 +59,16 @@ while True:
             print(f'Withdrew ${amount}')
         else:
             print('Insufficient funds')
+
     elif command == 'interest':
         amount = atm.calc_interest()  # call the calc_interest() method
         atm.deposit(amount)
         print(f'Accumulated ${amount} in interest')
+
     elif command == 'print':
         atm.print_transactions()
         print(f"Here are all your transactions: {transactions}")
+
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
@@ -69,7 +76,9 @@ while True:
         print('withdraw - withdraw money')
         print('interest - accumulate interest')
         print('exit     - exit the program')
+
     elif command == 'exit':
         break
+
     else:
         print('Command not recognized')
