c3213f8aad7618d50f467e7e86d416038396c03f
Scott Cormack <cormackscott@hotmail.com>
2021-08-09 19:07:09 -0400

    Complete lab 15

--- code/scott/lab15.py
+++ code/scott/lab15.py
@@ -17,16 +17,18 @@ lin_search(nums, 6)
 
 # Part 2 - Binary Search
 def bin_search(numbers, num):
-    length = len(numbers) - 1
-    low = numbers[0]
-    high = numbers[length]
-    while numbers[low] <= numbers[high]:
-        mid = ((numbers[low] + numbers[high]) // 2)
-        if numbers[mid] < numbers[num]:
-            numbers[low] = numbers[mid] + 1
-        elif numbers[mid] > numbers[num]:
-                numbers[high] = numbers[mid] - 1
+    low = 0
+    mid = 0
+    high = len(numbers) - 1
+    while high >= low:
+        mid = (high + low) // 2
+        if numbers[mid] < num:
+           low = mid + 1
+        elif numbers[mid] > num:
+           high = mid -1
         else:
             print(f'Your number was found at index {numbers.index(mid)}.')
+            break
+
 
 bin_search(nums, 6)
