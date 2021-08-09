feae2b0c96128ad10e898e9e649ecead900f897a
pjz987 <pwj2012@gmail.com>
2021-08-09 08:43:51 -0700

    random student setup

--- code/pete/random_student.py
+++ code/pete/random_student.py
@@ -1,4 +1,9 @@
 from random import choice
+import argparse
+
+parser = argparse.ArgumentParser(description='random students')
+parser.add_argument('groups', type=int, default=0, nargs='?', help='Number of groups', metavar='G')
+args = parser.parse_args()
 
 students = [
     'megan',
@@ -10,11 +15,20 @@ students = [
     'scott'
 ]
 
-while len(students) > 0:
-    student = choice(students)
-    students.remove(student)
-    if input(student):
-        break
+def random_students():
+    while len(students) > 0:
+        student = choice(students)
+        students.remove(student)
+        if input(student):
+            break
+
+def random_groups(n):
+    print(f'split into {n} groups')
+
+if args.groups != 0:
+    random_groups(args.groups)
+else:
+    random_students()
 
 # truthy_or_falsey_string = input(choice(students))
 # if truthy_or_falsey_string:
