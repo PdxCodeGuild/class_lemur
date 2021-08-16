8527b48d579aca08fbc10c40db05cbe6437d434a
meganbradner <meganrbradner@gmail.com>
2021-08-16 11:21:38 -0700

    made changes

--- code/megan/example.py
+++ code/megan/example.py
@@ -1,53 +1,46 @@
+from math import pi
 from easygui.boxes.derived_boxes import enterbox
+import easygui
 from mingus.core import chords
-from mingus.core.keys import get_key
 import mingus.core.notes as notes
 import mingus.core.progressions as progressions
 import random
 import mingus.core.intervals as intervals
+import mingus.core.scales as scales
 
 def random_notes(list):
     '''This function will choose a random letter from the scale list and return it'''
     return random.choice(list)
 
+def new_interval(note1, note2):
+    '''This function will take two notes and determine the interval between them'''
+    return intervals.determine(note1, note2)
+
 scale = ['A', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G','Ab']
 
 # ------------------------------------------------------------------------------------------- #
 '''
-Possible addition: 
+Possible additions: 
 -----------------
 
-Determine the note that is _ number of half steps away from a given note
+''' 
 
-while True: 
+pick_a_chord = random.choice(scale)
+print(pick_a_chord.split(' '))
 
-    note_one = random_notes(scale)
-    num = random.randint(1, 12)
+result  = scales.get_notes(pick_a_chord)
+print(result)
 
+while True:
 
-    while True: 
-        
-        solution = intervals.get_interval(note_one, num, key="C")
+    tonic_notes = input(f"Please enter the notes of the tonic scale in the key of {pick_a_chord}: ")
 
-        answer = input(f"What is the note that is {num} half steps above {note_one}? ")
+    tonic_notes = list(tonic_notes.split(' '))
 
-        if answer == 'done':
-            break
+    print(tonic_notes)
 
-        if answer == solution:
-            print("You got it!")
-
-            note_one = random_notes(scale)
-            num = random.randint(1, 12)
-
-            solution = intervals.get_interval(note_one, num, key="C")
-
-        else:
-            print(f"Sorry, not quite. The answer was {solution}")
-
-            note_one = random_notes(scale)
-            num = random.randint(1, 12)
-
-            solution = intervals.get_interval(note_one, num, key="C")
-                
-'''
+    if tonic_notes == result:
+        print("Genius!")
+        break
+    else:
+        print("Not quite")
--- code/megan/mini_capstone.py
+++ code/megan/mini_capstone.py
@@ -1,8 +1,9 @@
 import random
 from easygui.boxes.button_box import buttonbox
-from easygui.boxes.derived_boxes import enterbox, integerbox, msgbox
+from easygui.boxes.derived_boxes import enterbox, msgbox
 import mingus.core.keys as keys
 import mingus.core.intervals as intervals
+import mingus.core.scales as scales
 
 def random_notes(list):
     '''This function will choose a random letter from the scale list and return it'''
@@ -22,6 +23,7 @@ def relative_keys(list):
 
 
 scale = ['A', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G','Ab']
+options = ['I don\'t know', 'idk', 'not sure', 'i don\'t know', 'don\'t know']
 
 # ------------------------------------------------------------------------------------------ #
 
@@ -30,7 +32,7 @@ while True:
     msg = "What would you like to work on?"
     title = "Let's practice music theory!"
 
-    choices = ["Intervals", "Key signatures", "Relative minor keys", "Quit"]
+    choices = ["Intervals", "Key signatures", "Relative minor keys", "Scales", "Quit"]
     choice = buttonbox(msg, title, choices)
 
     if choice == 'Quit':
@@ -39,13 +41,10 @@ while True:
     elif choice == 'Intervals':
         note1 = random_notes(scale)
         note2 = random_notes(scale)
-        # both_notes = note1 + ' and ' + note2
-        # msgbox(both_notes)
 
         while True:
 
-            answer = enterbox(f"What is the interval between {note1} and {note2} \n(or 'done' to exit)")
-            # answer = enterbox("Please enter the interval between these two notes \n(or 'done' to exit): ")
+            answer = enterbox(f"What is the interval between {note1} and {note2} \n(or 'done' to exit) ")
 
             if answer == 'done':
                 break
@@ -55,32 +54,28 @@ while True:
 
                 note1 = random_notes(scale)
                 note2 = random_notes(scale)
-                # both_notes = note1, note2
-                # msgbox(both_notes)
 
-            elif answer == 'I don\'t know':
+            elif answer in options:
                 msgbox(f"The answer was {new_interval(note1, note2)}")
 
                 note1 = random_notes(scale)
                 note2 = random_notes(scale)
-                # both_notes = note1, note2
-                # msgbox(both_notes)
 
             elif answer != new_interval(note1, note2): 
-                msgbox("Sorry, not quite!")
+                msgbox("Sorry, try again!")
 
     elif choice == 'Key signatures':
 
-        signature = random_notes(scale) 
+        signature = random_notes(scale)
 
         while True: 
 
-            ask = enterbox(f"How many sharps or flats are in the key of {signature}? \nPositive numbers for sharp keys and negative numbers for flat keys \n(or 'done' to exit)")
+            ask = enterbox(f"How many sharps or flats are in the key of {signature}? \nPositive numbers for sharp keys and negative numbers for flat keys \n(or 'done' to exit) ")
 
             if ask == 'done':
                 break
 
-            elif ask == 'I don\'t know':
+            elif ask in options:
                 msgbox(f"The answer was {random_key_signature(signature)}")
                 
                 signature = random_notes(scale) 
@@ -109,23 +104,37 @@ while True:
                 
                 original_key = random_notes(scale)
 
-
-            elif new_key == 'I don\'t know':
+            elif new_key in options:
                 msgbox(f"The answer was {relative_keys(original_key)} minor")
                 
                 original_key = random_notes(scale)
 
             else:
                 msgbox("Sorry, try again!")
+    
+    elif choice == 'Scales':
+        
+        pick_a_chord = random_notes(scale)
 
+        while True:
 
-'''
+            tonic_notes = enterbox(f"Please enter the notes of the tonic scale in the key of {pick_a_chord} \n(or 'done' to exit) ")
 
-Features:
----------
+            if tonic_notes == 'done':
+                break
+            
+            elif tonic_notes in options:
+                msgbox(f"The answer was {scales.get_notes(pick_a_chord)}")
+    
+                pick_a_chord = random_notes(scale)
 
-- Display two notes and ask user what the interval between them is ---> DONE
-- Display key and ask for key signature ---> DONE
-- Display a key and ask for the parallel minor/major ---> DONE
+            tonic_notes = list(tonic_notes.split(' '))
 
-'''
+            if tonic_notes == scales.get_notes(pick_a_chord):
+                msgbox("You got it!")
+
+                pick_a_chord = random_notes(scale)
+
+            else:
+                msgbox("Sorry, try again!")
+
