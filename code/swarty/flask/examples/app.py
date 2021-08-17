eebc1ca7d593810266d13c60ae7e23a8fa777983
David Swartwood <david.swartwood@att.net>
2021-08-17 14:18:46 -0700

    renamed:    app.py -> examples/app.py 	renamed:    templates/index.html -> examples/templates/index.html 	renamed:    templates/random-color.html -> examples/templates/random-color.html

--- code/swarty/flask/examples/app.py
+++ code/swarty/flask/examples/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from random import choice
 app = Flask(__name__)
 
+colors=['red','green','blue','orange','indoio','purple']
+
 @app.route('/')
 def index(): # this is the index
     return render_template('index.html')
@@ -11,11 +13,14 @@ def colors():
     return "red, Green, & Blue"
 
 @app.route('random-color/')
-def random_color():
-    colors=['red','green','blue','orange']
+def random_color(colors=colors):
     color = choice(colors)
     return render_template('random-color.html', color=color)
 
+app.route('random-colors/<int:number>/')
+def random_colors(number, colors=colors):
+    rancolors=sample(colors, number)
+    return str(number)
 
 app.run(debug=True)
 
