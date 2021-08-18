e15452afa212f1710718a608d6279d852e495d3f
dbrunken <david.brunken@startmail.com>
2021-08-18 15:00:21 -0700

    flask app1

--- code/dbrunken/Python/dad_jokes.py
+++ code/dbrunken/Python/dad_jokes.py
@@ -2,9 +2,18 @@
 use API to get some dad jokes
 '''
 
+import pprint
 import requests
 
-while True:
-    url = 'https://icanhazdadjoke.com/'
 
-    headers = {'Accept' : 'application/json'}
+url = 'https://icanhazdadjoke.com/'
+headers = {'Accept' : 'application/json'}
+
+response = requests.get(url, headers=headers)
+
+# pprint.pprint(response.text)
+data = response.json()
+print(data['joke'])
+
+#----------------------------VERSION 2-------------------------------#
+
