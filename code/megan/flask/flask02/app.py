71721b67c83196fa6404c4f25e174d20d43d9a8f
meganbradner <meganrbradner@gmail.com>
2021-08-19 12:10:59 -0700

    made changes

--- code/megan/flask/flask02/app.py
+++ code/megan/flask/flask02/app.py
@@ -1,4 +1,6 @@
+from os import RTLD_NODELETE
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__) 
 
@@ -11,9 +13,10 @@ def unit_converter():
     if request.method == 'POST':
         print('you did a POST request')
         print(request.form)
-
-        distance = request.form.get('distance')
-        units = request.form.get('units')
+        data = dict(request.form)
+        print(data)
+        distance = int(data.get('distance'))
+        units = data.get('units')
 
         conversions = {
         'feet': 0.3048,
@@ -32,33 +35,33 @@ def unit_converter():
 
         rounded = round(convert,2)
 
-        return render_template('unit-converter.html', rounded=rounded)
+        return render_template('unit-converter.html', distance=distance, units=units, rounded=rounded)
+    
+    print('you did a GET request')
+    return render_template('unit-converter.html')
     
 
 @app.route('/anagram-checker/', methods=['GET', 'POST'])
 def anagram_checker():
     if request.method == 'POST':
         print('you did a POST request')
-        print(request.form)
+        data = dict(request.form)
+
+        first = data.get('first')
+        second = data.get('second')
+
+        first1 = sorted(first)
+        second2 = sorted(second)
+
+        first1 = ''.join(map(str, first1))
+        second2 = ''.join(map(str, second2))
         
-        first = request.form.get('first')
-        second = request.form.get('second')
-
-        first = list(first)
-        second = list(second)
-
-        first = first.sort()
-        second = second.sort()
-
-        if first == second:
-            True
+        if first1 == second2:
+            anagram_check = "are anagrams!"
         else:
-            False
+            anagram_check = "are not anagrams."
 
-        # anagram_check = ...
-
-        return render_template('anagram-checker.html')
-        # , anagram_check=anagram_check)
+        return render_template('anagram-checker.html', first=first, second=second, anagram_check=anagram_check)
 
     print('you did a GET request')
     return render_template('anagram-checker.html')
