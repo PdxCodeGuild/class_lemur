10b33aa7f7ec4484c9245841d62467e1e82a1950
meganbradner <meganrbradner@gmail.com>
2021-08-20 16:29:18 -0700

    made changes

--- code/megan/flask/flask03/app.py
+++ code/megan/flask/flask03/app.py
@@ -1,4 +1,3 @@
-from re import A
 from flask import Flask, render_template, request, redirect
 import json
 
@@ -14,21 +13,29 @@ def write_contacts(contacts):
     with open('contacts.json', 'w') as f:
         json.dump({'contacts': contacts}, f)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         contacts = read_contacts()
 
         info = dict(request.form)
-        print(info)
-
+        
         contacts.append(info)
-        print(contacts)
 
         write_contacts(contacts)
         return redirect('/')
 
     contacts = read_contacts()
     return render_template('index.html', contacts=contacts)
+    
+
+@app.route('/<name>/')
+def detail(name):
+
+    contacts = read_contacts()
+
+    return render_template('detail.html', contacts=contacts, name=name)
+
 
 app.run(debug=True)
