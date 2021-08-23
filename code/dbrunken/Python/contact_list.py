cf852bd052a8d1541017d446b58c5ad0fab94ff9
dbrunken <david.brunken@startmail.com>
2021-08-23 16:31:18 -0700

    work work work

--- code/dbrunken/Python/contact_list.py
+++ code/dbrunken/Python/contact_list.py
@@ -3,6 +3,17 @@ Contact list
 8/4
 create a contact list using a csv file
 '''
+'''
+contact_list = [
+    {'name': 'David Brunken', 'numbers': '91735-9374937', 'city': 'Lake Oswergo', 'occupation': 'Code Student'},
+    {'name': 'Micah Romani', 'numbers': '348713-9348', 'city': 'Lake Oswergo', 'occupation': 'Supply Delivery'},
+    {'name': 'Old Man', 'numbers': '32174-8748732', 'city': 'Canby', 'occupation': 'Pilot'},
+    {'name': 'Nick McKenzie', 'numbers': '6123-64012', 'city': 'Avondale', 'occupation': 'Life Insurance'},
+]
+'''
+
+from os import name
+
 
 with open('contact_list.csv', 'r') as file:
     contacts = file.read().split('\n')
@@ -25,29 +36,49 @@ print(contact_list)
 # print(contacts)
 
 
-'''
-contact_list = [
-    {'name': 'David Brunken', 'numbers': '91735-9374937', 'city': 'Lake Oswergo', 'occupation': 'Code Student'},
-    {'name': 'Micah Romani', 'numbers': '348713-9348', 'city': 'Lake Oswergo', 'occupation': 'Supply Delivery'},
-    {'name': 'Old Man', 'numbers': '32174-8748732', 'city': 'Canby', 'occupation': 'Pilot'},
-    {'name': 'Nick McKenzie', 'numbers': '6123-64012', 'city': 'Avondale', 'occupation': 'Life Insurance'},
-]
-'''
-
-
 while True:
-    user_in = input('Would you like to add any other contacts to the list? ')
+    user_in = input('Add, Retrieve, Update, or Delete from the list? ')
     no = 'no'
+    add = 'add'
+    update = 'update'
+    retrieve = 'retrieve'
+    delete = 'delete'
     if user_in == 'no':
         print('goodbye')
         break
-
-    user_name = input("\nWho would you like to add? ")
-    user_number = input("\nWhat's their number? ")
-    user_city= input("\nWhere do they live? ")
-    user_occ= input("\nWhat do they do? ")
-    user_dict = dict(zip(headers, user_name, user_number, user_city, user_occ))
-    contact_list.append(user_dict)
     
-    print('\n', contact_list)
+    #----------Add Contact-------------#
+    elif user_in == add:
+        user_name = input("\nWho would you like to add? ")
+        user_number = input("\nWhat's their number? ")
+        user_city= input("\nWhere do they live? ")
+        user_occ= input("\nWhat do they do? ")
+        user_dict = dict(zip(headers, [user_name, user_number, user_city, user_occ]))
+        contact_list.append(user_dict)
+        print('\n', contact_list)
+  
+    #----------Retrieve Contact---------#
+    elif user_in == retrieve:
+        fetch_name = input("\n Who's contact info do you want to see? ")
+        for contact in contact_list:
+            if contact['name'] == fetch_name.title():
+                print(contact)
+    
+    #----------Update Contact-----------#
+    elif user_in == update:
+        
+        pass
+    
+    #---------Delete Contact----------#
+    elif user_in == delete:
+        name_delete = input("\nWho needs to be removed? ")
+        # loop over list of dictionaries
+        for contact in contact_list: # contact is the loop variable
+            if contact['name'] == name_delete.title(): # contact['name'] matches what you want to delete
+                contact_list.remove(contact)
+                print(contact_list)
+
+    else:
+        print("don't recognize that command")
+
     
--- code/dbrunken/Python/peaks_valleys.py
+++ code/dbrunken/Python/peaks_valleys.py
@@ -10,18 +10,31 @@ pv_data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peak(data):
     peaks = []
-    highest_peak = max(data)
-    for i in range(len(data)):
-        if data[i] >= highest_peak:
-            peaks.append[i]
-    print(peaks)
+    for i in range(len(data[: -1])):
+        if i == 0:
+            continue
+        if data[i-1] < data[i] and data[i+1] < data[i]:
+            peaks.append(i)
+    # print(peaks)
+    return peaks
 
 def valley(data):
     valleys = []
-    lowest_valley = min(data)
-    for i in range(len(data)):
-        if data[i] <= lowest_valley:
-            valleys.append[i]
-    print(valleys)
+    for i in range(len(data[: -1])):
+        if i == 0:
+            continue
+        if data[i-1] > data[i] and data[i+1] > data[i]:
+            valleys.append(i)
+    # print(valleys)
+    return valleys
 
-# def peaks_valleys():
+def peaks_valleys(data):
+    pv_list = []
+    for i in range(len(data[1:-1])):
+        if data[i -1] == data[i + 1]:
+            pv_list.append(i)
+    return pv_list
+
+print(peak(pv_data))
+print(valley(pv_data))
+print(peaks_valleys(pv_data))
