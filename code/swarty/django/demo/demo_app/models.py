f1165b6d6f659e90ac15cfbea4ba89b6d760851d
David Swartwood <david.swartwood@att.net>
2021-09-07 16:22:15 -0700

    modified:   ../../demo/demo_app/models.py 	modified:   ../../demo/demo_app/templates/demo_app/index.html 	modified:   ../../demo/demo_app/views.py 	new file:   manage.py 	new file:   polls/__init__.py 	new file:   polls/admin.py 	new file:   polls/apps.py 	new file:   polls/migrations/0001_initial.py 	new file:   polls/migrations/__init__.py 	new file:   polls/models.py 	new file:   polls/tests.py 	new file:   polls/urls.py 	new file:   polls/views.py 	new file:   polls_proj/__init__.py 	new file:   polls_proj/asgi.py 	new file:   polls_proj/settings.py 	new file:   polls_proj/urls.py 	new file:   polls_proj/wsgi.py

--- code/swarty/django/demo/demo_app/models.py
+++ code/swarty/django/demo/demo_app/models.py
@@ -6,3 +6,6 @@ class City(models.Model):
     food = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     population = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- code/swarty/django/demo/demo_app/views.py
+++ code/swarty/django/demo/demo_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import City
 
 def index(request):
-    context= {'colors': ['red', 'green', 'blue'],}
+    cities = City.objects.all()
+    print(cities) # <QuerySet [<City: Portland>, <City: Rhodes>, <City: Seville>, <City: Seoul>]>
+    context = {
+        'cities': cities,
+    }
     return render(request, 'demo_app/index.html', context)
