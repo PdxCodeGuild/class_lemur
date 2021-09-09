f76bb4485603411a6fa08aa328e7c48c98b69f17
Scott Cormack <cormackscott@hotmail.com>
2021-09-09 19:26:57 -0400

    Scott - Grocery List

---
+++ code/scott/django/grocery_list/grocery_list/list/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Groceries(models.Model):
+    item = models.CharField('Grocery Item', max_length=50)
+    date_added = models.DateTimeField('Date Added', auto_now_add=True)
+    date_bought = models.DateTimeField('Date Bought', blank=True, null=True)
+    cleared = models.BooleanField('Added?', default=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.item
