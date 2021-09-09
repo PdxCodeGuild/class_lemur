92bf89eb3a1b3ba2e53eac392e42ca30ad5eb352
David Swartwood <david.swartwood@att.net>
2021-09-09 14:07:48 -0700

    modified:   list_app/models.py 	renamed:    list_app/templates/polls/detail.html -> list_app/templates/polls/delete.html 	deleted:    list_app/templates/polls/results.html

--- code/swarty/django/lab01/List_Proj/list_app/models.py
+++ code/swarty/django/lab01/List_Proj/list_app/models.py
@@ -5,7 +5,7 @@ import datetime
 class GroceryItem(models.Model):
     list_title = models.CharField(max_length=40)
     list_text = models.CharField(max_length=200)
-    creation_date = models.DateTimeField('date created')
-    comp_date=models.DateTimeField('date created')
+    creat_date = models.DateTimeField('date created')
+    comp_date=models.DateTimeField('date completed')
     def __str__(self):
         return self.list_title
