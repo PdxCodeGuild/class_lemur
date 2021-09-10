e8bd47e98f0419e8d1c18bb7e042950e4b90423b
meganbradner <meganrbradner@gmail.com>
2021-09-10 14:23:55 -0700

    made changes

--- code/megan/django/grocery_list/myapp/models.py
+++ code/megan/django/grocery_list/myapp/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class GroceryItem(models.Model):
     item = models.CharField(max_length=200)
     created_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
-    completed_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+    completed_date = models.DateField(null=True)
     complete = models.BooleanField(default=False)
 
     def __str__(self):
