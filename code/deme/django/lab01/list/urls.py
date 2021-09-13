f2d3141956e8603ef3af8c16d87fc6692878fe37
demesimms <simmons.demetric@gmail.com>
2021-09-13 19:35:47 -0400

    django labs

---
+++ code/deme/django/lab01/list/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path('', views.index),
+    # path('', admin.site.urls),
+    path('', views.index, name="index"),
+    path('', views.complete, name="complete"),
+    path('', views.delete, name = "delete")
+]
