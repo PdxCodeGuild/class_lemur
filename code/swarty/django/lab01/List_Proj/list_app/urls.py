fa94c4325f29d61d5f015c9c8afa62f9ebf92e85
David Swartwood <david.swartwood@att.net>
2021-09-14 16:30:49 -0700

    modified:   list_app/templates/list_app/index.html 	modified:   list_app/urls.py 	modified:   list_app/views.py

--- code/swarty/django/lab01/List_Proj/list_app/urls.py
+++ code/swarty/django/lab01/List_Proj/list_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'list_app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('complete', views.complete, name='complete'),
+    path('delete', views.delete, name='delete')
 ]
