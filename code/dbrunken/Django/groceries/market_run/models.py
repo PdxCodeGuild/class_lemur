145215255f8103a56cecb7654951c02d4ddfcd26
dbrunken <david.brunken@startmail.com>
2021-09-14 16:33:46 -0700

    commit 'groceries'

--- code/dbrunken/Django/groceries/market_run/models.py
+++ code/dbrunken/Django/groceries/market_run/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import uuid
 
 class Item(models.Model):
     item = models.CharField('Item', max_length=100)
--- code/dbrunken/Django/groceries/market_run/views.py
+++ code/dbrunken/Django/groceries/market_run/views.py
@@ -3,11 +3,11 @@ from .models import Item
 
 def index(request):
     items = Item.objects.all()
-
+    print(items)
     context = {
         'grocery_items' : items
     }
     return render(request, 'market_run/index.html', context)
 
-def base(request):
-    
+# def base(request):
+#     return render(request, 'market_run/base.html')
