ac7f4c215314f3539fcead403c464c3898bad7a9
pjz987 <pwj2012@gmail.com>
2021-09-16 11:49:59 -0700

    forms.py example

--- code/pete/django/examples/asoiaf_wiki/wiki/views.py
+++ code/pete/django/examples/asoiaf_wiki/wiki/views.py
@@ -4,25 +4,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import Character, House
 
 def index(request):
-    # get all objects of the Character model
-    # Django ORM
-    characters = Character.objects.all()
-    # SQL Query
-    # SELECT * FROM wiki_character
+    # # get all objects of the Character model
+    # # Django ORM
+    # characters = Character.objects.all()
+    # # SQL Query
+    # # SELECT * FROM wiki_character
 
-    # get all objects of the Character model who belong to House Stark
-    # Djano ORM
-    characters = Character.objects.filter(house_id=1)
-    # SQL Query
-    # SELECT * FROM wiki_character
-    # WHERE house_id = 1;
+    # # get all objects of the Character model who belong to House Stark
+    # # Djano ORM
+    # characters = Character.objects.filter(house_id=1)
+    # # SQL Query
+    # # SELECT * FROM wiki_character
+    # # WHERE house_id = 1;
 
-    # get all objects of the Character model with Stark in the name
-    # Django ORM
-    characters = Character.objects.filter(name__ends_with='Stark')
-    # SQL Query
-    # SELECT * FROM wiki_character
-    # WHERE name LIKE '%Stark';
+    # # get all objects of the Character model with Stark in the name
+    # # Django ORM
+    # characters = Character.objects.filter(name__ends_with='Stark')
+    # # SQL Query
+    # # SELECT * FROM wiki_character
+    # # WHERE name LIKE '%Stark';
 
     characters = Character.objects.order_by('house', 'name')
 
