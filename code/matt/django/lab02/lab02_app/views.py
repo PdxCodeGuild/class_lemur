a9d281a6447917520cb8f544ff367771b2fda604
mbKirby <matthew.kirby90@yahoo.com>
2021-09-17 19:19:05 -0400

    modified django lab02 and added django lab03

--- code/matt/django/lab02/lab02_app/views.py
+++ code/matt/django/lab02/lab02_app/views.py
@@ -1,42 +1,43 @@
 from django.shortcuts import redirect, render, get_object_or_404
 
-from .models import Author, Book, Checkout
+from .models import Author, Book, Checkout, Genre
 
 
 def index(request):
     authors = Author.objects.order_by('name')
     books = Book.objects.all
     checkout = Checkout.objects.all
+    genre = Genre.objects.all
+
     if request.method == 'POST':
         if 'checkout' in request.POST:
             book = Book.objects.filter(title=request.POST.get('book'))[0]
             user = request.POST.get('user_name')
             time = request.POST.get('checkout_time')
             checkouts = Checkout.objects.filter(checkout=True)
-            for book1 in checkouts:
-                if book != book1:
-                    Checkout.objects.create(
-                        book=book,
-                        user=user,
-                        checkout=True,
-                        time=time,
-                    )
+            for checkout_book in checkouts:
+                if book == checkout_book.book:
+                    author = book.author
+                    context = {
+                        'book': book,
+                        'author': author
+                    }
+                    return render(request, 'lab02_app/checkout.html',  context)
 
-        if 'checkin' in request.POST:
-            books = Checkout.objects.filter(checkout=True)
+            Checkout.objects.create(
+                book=book,
+                user=user,
+                checkout=True,
+                time=time,
+            )
 
-            for book in books:
-                check = request.POST.get(f'{book.book}checkout')
-                if check:
-                    book.checkout = False
-                    book.save()
-
-        return redirect("/")
+        return redirect("lab02_app:index")
 
     context = {
         'authors': authors,
         'books': books,
-        'checkout': checkout
+        'checkout': checkout,
+        'genre': genre
     }
     return render(request, 'lab02_app/index.html', context)
 
@@ -44,3 +45,10 @@ def index(request):
 def checkout(request, name):
     author = get_object_or_404(Author, name=name)
     return render(request, 'lab02_app/checkout.html', {'author': author})
+
+
+def checkin(request, id):
+    book = get_object_or_404(Checkout, id=id)
+    book.checkout = False
+    book.save()
+    return redirect('lab02_app:index')
