694aac2bf75c5a66a99f0cdc3aeac422401d1cbb
demesimms <simmons.demetric@gmail.com>
2021-09-22 17:16:32 -0400

    updated lab01 js

---
+++ code/deme/js/lab01/average_numbers.js
@@ -0,0 +1,30 @@
+let nums = [5, 0, 8, 3, 4, 1, 6]
+let sum = 0
+
+for (let i = 0;  i < nums.length; i++) {
+    sum += nums[i]
+    console.log(nums[i])
+}
+console.log(sum)
+
+let averageNum = sum / nums.length
+console.log(averageNum)
+
+let div1 = document.querySelector('#numbers')
+div1.innerText = "Hello Worldu!"
+
+
+// let userNums=[]
+// let userInput = prompt("Please enter a number or 'done' to exit.")
+// while(userInput != 'done'){
+//   userNums.push(userInput)
+//   userInput = prompt("Please enter a number or 'done' to exit.")
+// }
+
+// console.log(userNums)
+// console.log(userInput)
+
+
+
+
+
