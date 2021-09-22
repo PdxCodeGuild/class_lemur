ff870ca42e89d2b81b5dd65301e86a344605bb28
David Swartwood <david.swartwood@att.net>
2021-09-22 09:33:48 -0700

    modified:   app_chirp/models.py

--- code/swarty/django/lab03/app_chirp/models.py
+++ code/swarty/django/lab03/app_chirp/models.py
@@ -7,8 +7,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     body = models.CharField(max_length=288)
     time=models.DateTimeField(auto_now_add=True)
-    author=models.ForeignKey(User, on_delete=models.PROTECT)
+    author=models.ForeignKey(User, on_delete=models.PROTECT, related_name='posts') # user.posts.all() QuerySet of all that user's posts
     archive = models.BooleanField(default=False)
     archive_date=models.DateTimeField(null=True,blank=True)
     def __str__(self):
         return f'{self.author},{self.time},\n {self.body}'        
+
+# {% for post in user.posts.all %}
+# {{ post }}
+# {% endfor %}
