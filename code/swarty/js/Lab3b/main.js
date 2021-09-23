1ba0e2237f70ef00f71371f706756645b746cf2b
David Swartwood <david.swartwood@att.net>
2021-09-23 13:59:12 -0700

    deleted:    code/swarty/js/Lab2a/main.js 	renamed:    code/swarty/js/Lab2a/index.html -> code/swarty/js/Lab2b/index.html 	new file:   code/swarty/js/Lab2b/main.js 	new file:   code/swarty/js/Lab3b/index.html 	new file:   code/swarty/js/Lab3b/main.js

---
+++ code/swarty/js/Lab3b/main.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('#button')
+
+
+const input1 = document.querySelector('#cash')
+
+
+const Output = document.getElementById('output')
+const cents = [
+	{name:"tens", value: 1000},
+    {name:"fives", value: 500},
+    {name:"ones", value:100},
+	{name:"half-dollars", value: 50},
+	{name:"quarters", value:25},
+	{name:"dimes", value: 10},
+	{name:"nickels", value: 5},
+	{name:"pennies", value: 1}
+]
+// console.log(cents.length)
+function count(x, y){
+    let div= Math.floor((x)/y)
+    // console.log(div,"counting")
+    return div
+}
+function rem(x, y){
+    let rmdr=Math.round((x)%y)
+    // console.log(rmdr,"rmdr")
+    return rmdr
+}
+
+button.addEventListener('click', function () {
+    Output.innerText ="Your total is: \n"
+    let cash=100*input1.value
+    for (let i = 0; i< cents.length; i++){
+        const coin=cents[i]
+        let counted = count(cash,coin.value) 
+        cash= rem(cash,coin.value)
+        Output.innerText+= `${counted} ${coin.name}\n`
+    }
+})
