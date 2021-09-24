36492422e0a98d62a503890a68a5bccddac1d268
demesimms <simmons.demetric@gmail.com>
2021-09-24 16:56:16 -0400

    finished average numbers lab for JS

--- code/deme/js/lab01/average_numbers.js
+++ code/deme/js/lab01/average_numbers.js
@@ -1,9 +1,24 @@
+//                  Version 1
+let numArray = [5, 0, 8, 3, 4, 1, 6]
+let total = 0
+let version1 = document.getElementById('version1')
+
+for (let i = 0;  i < numArray.length; i++) {
+    total = total + +numArray[i]
+    console.log(numArray[i])
+}
+averageNum = document.createElement('p')
+averageNum.innerHTML =(`For [5, 0, 8, 3, 4, 1, 6], the median is number is ${total / numArray.length}`)
+version1.appendChild(averageNum)
+
 //                  Version2
 let numForm = document.getElementById('numForm')
 let numList = document.getElementById('numList')
 let numListForm = document.getElementById('numListForm')
 let numbers = document.getElementById('numbers')
-let num = []
+let nums = []
+let doneForm = document.getElementById('doneForm')
+let sum = 0
 
 
 numForm.addEventListener('submit', function(event) {
@@ -12,38 +27,16 @@ numForm.addEventListener('submit', function(event) {
     let numLi = document.createElement('li')
     numLi.innerHTML=userNum
     numList.appendChild(numLi)
-    })
+    nums.push(numListForm.value)
 
-    let done = document.createElement('button')
-    done.innerText = 'Done'
-    numForm.appendChild(done)
-
-    done.addEventListener('click', function(){
-        for(let i =0; i<numList.length; i++)
-        nums.push(numList[i])
-        console.log(nums)
-    })
+})
+doneForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    for(let i = 0; i < nums.length; i++){
+        sum = sum + +nums[i]
+    }
+    sum = sum/nums.length
+    alert(`The average number is ${sum}`)
 })
 
-// let userNums=[]
-// let userInput = prompt("Please enter a number or 'done' to exit.")
-// while(userInput != 'done'){
-//   userNums.push(userInput)
-//   userInput = prompt("Please enter a number or 'done' to exit.")
-// }
 
-
-// console.log(userNums)
-// console.log(userInput)
-
-// let nums = [5, 0, 8, 3, 4, 1, 6]
-// let sum = 0
-
-// for (let i = 0;  i < nums.length; i++) {
-//     sum += nums[i]
-//     console.log(nums[i])
-// }
-// console.log(sum)
-
-// let averageNum = sum / nums.length
-// console.log(averageNum)
