84263c2ab15d20e94b6dfac734a4cb2aca76294d
meganbradner <meganrbradner@gmail.com>
2021-09-28 13:32:56 -0700

    made changes

---
+++ code/megan/javascript/lab05/todos/js/site.js
@@ -0,0 +1,52 @@
+const App = {
+    data() {
+        return {
+            items: [
+                {
+                    title: 'Buy milk',
+                    isComplete: false
+                },
+                {
+                    title: 'Get gas',
+                    isComplete: true
+                }
+            ],
+            newItem: '',
+        }
+    },
+    computed: {
+        itemFalse() {
+            return this.items.filter(item => item.isComplete === false)
+        },
+        itemTrue() {
+            return this.items.filter(item => item.isComplete === true)
+        }
+    },
+    methods: {
+
+        addItem: function() {
+
+            this.items.push({
+                title: this.newItem,
+                isComplete: false
+            })
+            
+            this.newItem = ''
+        },
+
+        markCompleted: function (item) {
+
+            item.isComplete = true
+        },
+
+        removeItem: function (index) {
+            
+            this.items.splice(this.items.indexOf(item), 1)  
+            // ^ removes the last item of the list 
+
+        }
+
+    }
+}
+
+Vue.createApp(App).mount('#app')
