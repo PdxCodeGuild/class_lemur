1cfe598d98ab70280aff02146297534f125a661c
David Swartwood <david.swartwood@att.net>
2021-10-07 14:28:05 -0700

    modified:   code/swarty/js/Lab5/index.html 	modified:   code/swarty/js/Lab5/js/main.js

--- code/swarty/js/Lab5/js/main.js
+++ code/swarty/js/Lab5/js/main.js
@@ -2,24 +2,31 @@ const App = {
     data() {
         return {
             message: 'To do List',
-            list: [
-            ],
+            list: [],
             inputText: '',
+            checkedNames:[],
+            incomplete:[],
         }
     },
     methods: {
         submitForm() {
             this.list.push(this.inputText)
             this.inputText=""
+            console.log(this.list)
         },
         deleteItem(item){
-            let i=0
-            console.log(item, "item")
+            i=0
+            j=0
             i=this.list.indexOf(item)
-            console.log(i, "index" , this.list)
-            this.list.splice(i,1)
-            console.log(this.list,"new")
-        }
+            this.list.splice(i,1) 
+            console.log(this.checkedNames.indexOf(item))
+            if (this.checkedNames.indexOf(item)>=0){
+                j=this.checkedNames.indexOf(item)
+                this.checkedNames.splice(j,1)
+            }
+
+            console.log(this.checkedNames, this.list)
+        },
     },
     
     // created() {
