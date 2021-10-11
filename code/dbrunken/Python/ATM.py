2c4ba67dd736ea875472f8260b3332cc70f6439e
dbrunken <david.brunken@startmail.com>
2021-10-11 09:44:37 -0700

    completed labs

--- code/dbrunken/Python/ATM.py
+++ code/dbrunken/Python/ATM.py
@@ -4,15 +4,18 @@ using a <class>, create an ATM
 '''
 
 class ATM:
-    def __init__(self, balance = 0, interest_rate= -.4):
+    def __init__(self, balance = 0, interest_rate= .1, transactions = []):
         self.balance = balance
         self.interest_rate = interest_rate
+        self.transactions = transactions
 
     def check_balance(self):
          return self.balance
 
     def deposit(self, amount):
         self.balance += amount
+        # self.transactions.append(+)
+        self.transactions.append(amount)
         # print()
         
     def check_withdrawl(self, amount):
@@ -25,23 +28,29 @@ class ATM:
 
     def withdrawl(self, amount):
         self.balance -= amount
+        # self.transactions.append(-)
+        self.transactions.append(amount)
         return self.balance
 
     def calc_interest(self):
         interest = self.balance * self.interest_rate
         self.balance += interest
         return self.balance
+    
+    def call_transactions(self):
+        print(self.transactions)
 
 atm = ATM()
-print("Welcome to Communist Chase ATM")
+print("\n Welcome to Bank ATM")
 while True:
-    command = input("Select a command: ")
+    command = input("\n Select a command: ")
     if command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
         print('deposit  - deposit money')
         print('withdraw - withdraw money')
         print('interest - accumulate interest')
+        print('transactions- check recent transactions')
         print('exit     - exit the program')
     
     elif command == 'balance':
@@ -60,11 +69,17 @@ while True:
             print(f'Withdrew ${amount}')
         else:
             print('Cant afford it')
+    
     elif command == 'interest':
         amount = atm.calc_interest()
         # atm.deposit(amount)
         print(f"New balance is ${amount} from interest owed")
+    
+    elif command == 'transactions':
+        print(atm.call_transactions())
+
     elif command == 'exit':
         break
+    
     else:
         print('Command not recognized')
--- code/dbrunken/Python/dad_jokes.py
+++ code/dbrunken/Python/dad_jokes.py
@@ -9,11 +9,29 @@ import requests
 url = 'https://icanhazdadjoke.com/'
 headers = {'Accept' : 'application/json'}
 
-response = requests.get(url, headers=headers)
+# response = requests.get(url, headers=headers)
 
-# pprint.pprint(response.text)
-data = response.json()
-print(data['joke'])
+# # pprint.pprint(response.text)
+# data = response.json()
+# print(data['joke'])
 
 #----------------------------VERSION 2-------------------------------#
 
+while True:
+    query = input('Enter keyword to search by: ').upper()
+    if query == '':
+        print('no jokes for you, which is better anyway')
+        break
+
+    url = f'https://icanhazdadjoke.com/search?term={query}'
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    data = data['results']
+
+    for i in data:
+        print(i['joke'])
+        anuthaOne = input('press enter to get more jokes or "exit" to stop: ')
+        if anuthaOne == 'exit':
+            print('no more jokes for you')
+            break
