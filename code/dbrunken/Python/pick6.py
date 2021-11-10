e698343bc9d5e57bb168eca3bf77c46621e5c16b
dbrunken <david.brunken@startmail.com>
2021-11-10 10:25:36 -0800

    pick6

--- code/dbrunken/Python/pick6.py
+++ code/dbrunken/Python/pick6.py
@@ -8,37 +8,57 @@ matching numbers between ticket and winning numbers determines payoff
 import random
 
 #got help from https://www.tutorialspoint.com/generating-random-number-list-in-python
+
 winning_numbers = []
-for i in range(6):
-    nums = random.randint(0, 99)
-    winning_numbers.append(nums)
-print(winning_numbers)
-
 drawn_numbers = []
-for i in range(6):
-    nums = random.randint(0, 99)
-    drawn_numbers.append(nums)
-print(drawn_numbers)
+balance = 0
+ticket_cost = 2
+num_games = 100000
 
-def match_payout():
-    matches = 0
-    pay_key = {
-        1 : 4,
-        2 : 7,
-        3 :100,
-        4 :50000,
-        5 :1000000,
-        6: 25000000
-    }
-    if pay_key(matches) == 6:
-        return print("Jack Pot!")
-    else:
-        return pay_key(matches)
+''' Winning numbers function '''
+
+def make_nums(ticket):
+    for i in range(6):
+        nums = random.randint(0, 99)
+        ticket.append(nums)
+    return ticket
 
 
-def lotto():
-    play_money = 0
-    jP = winning_numbers
-    for i in range(1000):
-        win_list = winning_numbers
+def check_ticket(drawn, winning):
+    wins = 0
+    for i in range(len(drawn)):
+        if drawn[i] == winning[i]:
+            wins += 1
+    return wins
 
+
+def match_payout(win_nums, winnings):
+    if win_nums == 6:
+        winnings += 25000000
+    elif win_nums == 5:
+        winnings += 1000000
+    elif win_nums == 4:
+        winnings += 500000
+    elif win_nums == 3:
+        winnings += 100
+    elif win_nums == 2:
+        winnings += 7
+    elif win_nums == 1:
+        winnings += 4
+    return winnings
+
+i =0
+while i < num_games:
+    winning_numbers = make_nums(winning_numbers)
+    drawn_numbers = make_nums(drawn_numbers)
+    wins = check_ticket(drawn_numbers,winning_numbers)
+    balance = match_payout(wins, balance)
+    winning_numbers =[]
+    drawn_numbers =[]
+    i += 1
+
+expenses = num_games * ticket_cost
+final_earning = balance - expenses
+roi = final_earning / expenses
+print(f'You spent ${expenses}, with a final balance of ${final_earning}')
+print(f'ROI: {roi}')
