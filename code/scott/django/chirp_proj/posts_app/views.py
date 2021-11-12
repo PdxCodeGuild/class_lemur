5bdd14431a2c8e02b3327a5b19e5bc4976ba208b
Scott Cormack <cormackscott@hotmail.com>
2021-11-12 15:12:36 -0500

    Forced push to ensure everything is up to date on Git

--- code/scott/django/chirp_proj/posts_app/views.py
+++ code/scott/django/chirp_proj/posts_app/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from .models import Post
-from .forms import PostForm
 
 # Create your views here.
 def feed(request):
@@ -9,17 +8,3 @@ def feed(request):
         'posts':posts,
     }
     return render(request, 'posts_app/feed.html', context)
-
-# def post(request):
-#     form = PostForm()
-#     context = {
-#         'form':form,
-#     }
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.poster = request.user
-#             post.save()
-#             return redirect('posts_app/feed.html', context)
-#     return render(request, 'posts_app/feed.html', context)
