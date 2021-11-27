bbfda5a31bc643e1d27d39076f996c6565ebfd1f
demesimms <simmons.demetric@gmail.com>
2021-11-26 20:09:23 -0500

    vue todo

--- code/deme/js/lab05/site.js
+++ code/deme/js/lab05/site.js
@@ -2,25 +2,35 @@ new Vue({
     el: '#app',
     data: {
         message: 'Todo List',
-        todo: [],
-        completed: [],
+        todo: [{
+            index: 1,
+            name: "",
+            isComplete: false,
+          }],
+        completed: [{
+            index: 1,
+            name: '',
+            isComplete: true,
+          }],
         addNewItem: '',
-
     },
     methods: {
-        i=0,
+
         addToListForm() {
-            this.todo.push({newItem: this.addNewItem})
+            this.todo.push({name: this.addNewItem, isComplete: false, index: this.todo.length +1 });
+            this.addNewItem='';
         },
-        Remove () {
-            this.todo.splice({newItem: this.addNewItem}, 1)
-            this.completed.splice({newItem: this.addNewItem}, 1)
+        Remove: function (item) {
+            this.todo = this.todo.filter((comItem) => comItem.name !== item.name)
+            this.completed = this.completed.filter((comItem) => comItem.name !== item.name)
 
           },
-        Complete () {
-            this.completed.push({newItem: this.addNewItem })
-            this.todo.splice({newItem: this.addNewItem }, 1)
+        Complete: function (item) {
+            item.isComplete = true
+            this.completed.push({name: item.name, index: this.length + 1, isComplete: true});
+            this.todo = this.todo.filter((comItem) => comItem.name !== item.name)
         },
+
     }
- //index for splice
+ 
 })
