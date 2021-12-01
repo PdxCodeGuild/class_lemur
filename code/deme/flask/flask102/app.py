dc97386105b4a32a7b3cf94d9a8e764acc26eec5
demesimms <simmons.demetric@gmail.com>
2021-12-01 10:50:52 -0500

    flask 102 and chirp

--- code/deme/flask/flask102/app.py
+++ code/deme/flask/flask102/app.py
@@ -17,6 +17,9 @@ def unit_converter():
 
 @app.route('/anagram/', methods=['GET', 'POST'])
 def anagram():
+    first_word = ""
+    second_word = ""
+    anagram = ""
     if request.method=='POST':
         print(request.form)
         data = dict(request.form)
@@ -35,37 +38,6 @@ def anagram():
             anagram= "not anagrams"
     return render_template('anagram.html', first_word=first_word, second_word=second_word, anagram = anagram)
     
-    
-    
-  
-    
-
-#     # distance = input("What is the distance in feet? ")
-#     # print(f"{distance} ft is {round(int(distance) * mea['feet'], 4)} m") 
-#     #used the round function here because it started returning a bunch of zeros at the end
-#     dist = input("what is the distance? ")
-#     unit = input("what are the units? ")
-#     print(f"{dist} {(unit)} is {int(int(dist) * mea[unit])}")
-#     return render_template('anagram.html')
-
-
-
-# first_word = input("Enter the first word: ")
-# first_word_split = list(first_word)
-# print(first_word_split)
-# second_word = input("Enter the second word: ")
-# second_word_split = list(second_word)
-# print(second_word_split)
-# first_word_split.sort()
-# second_word_split.sort()
-# if first_word_split == second_word_split:
-#     print(f"{first_word} and {second_word} are anagrams.")
-# else:
-#     print(f"{first_word} and {second_word} are not anagrams.")
-    
-
-
-
 
 
 app.run(debug=True)
